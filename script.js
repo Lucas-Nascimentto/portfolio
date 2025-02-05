83d571d2576f0e68d564980136ebfacd40f2f4a9
Lucas Almeida <la7371180@gmail.com>
2025-02-04 22:02:33 -0300

    Adicionando arquivos

---
+++ script.js
@@ -0,0 +1,4 @@
+function imprimirCurriculo() {
+    const url = './assets/Currículo-Lucas-Nascimento.pdf'; // Caminho do PDF
+    const win = window.open(url, '_blank'); // Abre o PDF em uma nova aba
+}
